c907a71bd3412d5aac791c9bc2e6bd7e9ee2ea28
Kevszzz <kevinromero>
2023-09-07 20:40:13 +0800

    Anchorpane Changes

--- src/main/java/com/example/biopayrollmanagementsystem/HelloController.java
+++ src/main/java/com/example/biopayrollmanagementsystem/HelloController.java
@@ -1,14 +1,12 @@
 package com.example.biopayrollmanagementsystem;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+public class HelloController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
